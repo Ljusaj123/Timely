bb8f5493309512381cec54d4553303d1bf048fdf
Ljusaj123 <ljusajrosana@gmail.com>
2022-10-29 15:22:04 +0200

    styles to the table, get request to display data

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core'
+import { ProjectService } from 'src/app/services/project-service.service'
+import { Project } from 'src/model/project'
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
+  projects: Project[] = []
 
-  constructor() { }
+  constructor(private projectService: ProjectService) {}
 
   ngOnInit(): void {
+    this.projectService
+      .getProjects()
+      .subscribe((result: Project[]) => (this.projects = result))
   }
-
 }
