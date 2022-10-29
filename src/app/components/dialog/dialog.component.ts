874987cf554f68284bbd41c4f82508344aec6d67
Ljusaj123 <ljusajrosana@gmail.com>
2022-10-29 13:54:49 +0200

    added styles, created structure

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-dialog',
@@ -6,7 +7,27 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./dialog.component.css'],
 })
 export class DialogComponent implements OnInit {
+  faTimes = faTimes
+  projectName: string = ''
+  errorMessage = ''
   constructor() {}
 
   ngOnInit(): void {}
+
+  closeDialog() {
+    const dialog = document.querySelector(
+      '.dialog-container',
+    ) as HTMLElement | null
+    if (dialog != null) dialog.style.display = 'none'
+    this.projectName = ''
+  }
+  handleDialog(name: string) {
+    if (name == '') {
+      this.errorMessage = 'Enter the name of the project'
+      return
+    }
+    this.errorMessage = ''
+    this.closeDialog()
+    this.projectName = name
+  }
 }
