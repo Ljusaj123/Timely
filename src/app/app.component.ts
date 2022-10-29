45a60606bc6a8e603657c950556c7ad308e4bb75
Ljusaj123 <ljusajrosana@gmail.com>
2022-10-29 15:08:26 +0200

    formated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'timely-UI';
+  title = 'timely-UI'
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
