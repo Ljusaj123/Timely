1a61a98585639027a324e4d061fda207038b5418
Ljusaj123 <ljusajrosana@gmail.com>
2022-10-29 15:05:41 +0200

    imported components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser'
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component'
+import { ButtonComponent } from './components/button/button.component'
+import { DialogComponent } from './components/dialog/dialog.component'
+import { TableComponent } from './components/table/table.component'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { ProjectsComponent } from './components/projects/projects.component'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    DialogComponent,
+    TableComponent,
+    ProjectsComponent,
+  ],
+  imports: [BrowserModule, HttpClientModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
