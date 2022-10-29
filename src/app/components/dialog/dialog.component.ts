f8d3614c28c17ac264728651e708c39e15243a87
Ljusaj123 <ljusajrosana@gmail.com>
2022-10-30 01:30:54 +0200

    created output decorator variable, added initialization project variable

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, EventEmitter, OnInit, Output } from '@angular/core'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { Project } from 'src/model/project'
 
 @Component({
   selector: 'app-dialog',
@@ -7,9 +8,12 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
   styleUrls: ['./dialog.component.css'],
 })
 export class DialogComponent implements OnInit {
+  @Output() projectName = new EventEmitter<string>()
+  project!: string
+
   faTimes = faTimes
-  projectName: string = ''
   errorMessage = ''
+
   constructor() {}
 
   ngOnInit(): void {}
@@ -19,15 +23,17 @@ export class DialogComponent implements OnInit {
       '.dialog-container',
     ) as HTMLElement | null
     if (dialog != null) dialog.style.display = 'none'
-    this.projectName = ''
+    this.project = ''
   }
+
   handleDialog(name: string) {
     if (name == '') {
       this.errorMessage = 'Enter the name of the project'
       return
     }
+
     this.errorMessage = ''
     this.closeDialog()
-    this.projectName = name
+    this.projectName.emit(name)
   }
 }
