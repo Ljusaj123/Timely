045aa48e7ac6831b69861fb512c17268131587f4
Ljusaj123 <ljusajrosana@gmail.com>
2022-10-30 03:14:14 +0100

    created service to update and delete project, refactored classes and styles

--- src/app/components/edit/edit.component.ts
+++ src/app/components/edit/edit.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
+import { ProjectService } from 'src/app/services/project-service.service'
+import { Project } from 'src/model/project'
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  styleUrls: ['./edit.component.css'],
 })
 export class EditComponent implements OnInit {
+  @Input() project!: Project
+  @Output() projectsUpdated = new EventEmitter<Project[]>()
 
-  constructor() { }
+  constructor(private projectService: ProjectService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  updateHero(project: Project) {
+    this.projectService
+      .updateProject(project)
+      .subscribe((result: Project[]) => this.projectsUpdated.emit(result))
   }
 
+  deleteHero(project: Project) {
+    this.projectService
+      .deleteProject(project)
+      .subscribe((result: Project[]) => this.projectsUpdated.emit(result))
+  }
 }
