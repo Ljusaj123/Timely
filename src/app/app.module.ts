ed21fcab7b9eeb513ad7a1cf4f2cd55936003276
Ljusaj123 <ljusajrosana@gmail.com>
2022-10-30 03:14:51 +0100

    formsmodule imported, todo list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component'
 import { ButtonComponent } from './components/button/button.component'
 import { DialogComponent } from './components/dialog/dialog.component'
 import { TableComponent } from './components/table/table.component'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-import { ProjectsComponent } from './components/projects/projects.component';
+import { ProjectsComponent } from './components/projects/projects.component'
 import { EditComponent } from './components/edit/edit.component'
 
 @NgModule({
@@ -19,8 +20,15 @@ import { EditComponent } from './components/edit/edit.component'
     ProjectsComponent,
     EditComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, FontAwesomeModule],
+  imports: [BrowserModule, HttpClientModule, FontAwesomeModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+/*TO DO:
+
+1. FIX ON START BUTTON TO UPDATE TABLE
+2. PAGINATION
+3. EXPORT TO EXCEL
+*/
